491d46993c53c5be48b1a0aab66fefdae1733ad5
alireza <alireza.abdolalipoor@gmail.com>
2020-02-12 01:21:20 +0330

    add build src and di module - start refactoring

---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,12 @@
+
+object Deps {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    private object Versions {
+        val kotlin = "1.3.50"
+        val appCompat = "1.1.0"
+        val dagger = "2.25.2"
+    }
+}
